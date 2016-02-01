a72bad6f801251c951595a609143cbfed6894a73
Dwayne Schultz <dschultz@pivotal.io>
2016-01-31 20:33:43 -0700

    Restore NAT functionality - add support for amazon linux to bootstrap

--- common/bootstrap.sh
+++ common/bootstrap.sh
@@ -2,26 +2,65 @@
 
 set -e -x
 
-apt-get update
-apt-get --yes install python-pip python-dev libffi-dev libssl-dev git
-pip install 'requests[security]' --upgrade
-pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+FLAVOR=$(bash -c "source /etc/os-release && echo \$ID")
+case $FLAVOR in
+"ubuntu")
+    OS_USER=ubuntu
+    USER_HOME=/home/ubuntu
+    AWS_DIR=/usr/local/bin
+    ;;
+"amzn")
+    OS_USER=ec2-user
+    USER_HOME=/home/ec2-user
+    AWS_DIR=/opt/aws/bin
+    ;;
+*)
+    echo "Unknown Linux flavor $FLAVOR"
+    exit -1
+    ;;
+esac
 
 aws-error-exit() {
     REASON=$1
-    cfn-signal --exit-code 1 --reason \"$REASON\" \"$HANDLE\"
+    $AWS_DIR/cfn-signal --exit-code 1 --reason "$REASON" "$HANDLE"
     exit 1
 }
 
-cfn-init --stack $STACK_NAME --resource $RESOURCE --region $REGION || aws-error-exit 'Failed to run cfn-init'
-cfn-hup || aws-error-exit 'Failed to start cfn-hup'
+aws-signal-success() {
+    $AWS_DIR/cfn-signal --exit-code 0 --reason "$RESOURCE setup complete" "$HANDLE"
+}
 
-echo $HOSTNAME > /etc/hostname
-sed -i -e "s/127.0.0.1 localhost/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
+aws-bootstrap() {
+    case $FLAVOR in
+    "ubuntu")
+        # Can't use aws-error-exit here since AWS CLI hasn't been installed yet
+        apt-get update
+        apt-get --yes install python-pip python-dev libffi-dev libssl-dev git
+        pip install 'requests[security]' --upgrade
+        pip install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+        ;;
+    "amzn")
+        yum -y update || aws-error-exit 'yum update failed'
+        yum -y install git || aws-error-exit 'yum install extras failed'
+        ;;
+    esac
 
-git clone --depth=1 https://github.com/Bash-it/bash-it.git /root/.bash_it
-sudo -u ubuntu git clone --depth=1 https://github.com/Bash-it/bash-it.git /home/ubuntu/.bash_it
+    $AWS_DIR/cfn-init --stack $STACK_NAME --resource $RESOURCE --region $REGION || aws-error-exit 'Failed to run cfn-init'
+    $AWS_DIR/cfn-hup || aws-error-exit 'Failed to start cfn-hup'
 
-curl --output /tmp/bashrc.sh --silent $S3_URL/common/bashrc.sh
-cat /tmp/bashrc.sh >> /root/.bashrc
-cat /tmp/bashrc.sh >> /home/ubuntu/.bashrc
+    echo $HOSTNAME > /etc/hostname
+    echo "127.0.0.1 $HOSTNAME $HOSTNAME.localdomain localhost localhost.localdomain" > /etc/new-hosts
+    grep -v 127.0.0.1 /etc/hosts >> /etc/new-hosts || true
+    mv /etc/new-hosts /etc/hosts
+
+    if [[ $FLAVOR == "amzn" ]] ; then
+        sed -i -e "s/localhost/$HOSTNAME/g" /etc/sysconfig/network
+    fi
+
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git /root/.bash_it
+    sudo -u $OS_USER git clone --depth=1 https://github.com/Bash-it/bash-it.git $USER_HOME/.bash_it
+
+    curl --output /tmp/bashrc.sh --silent $S3_URL/common/bashrc.sh
+    cat /tmp/bashrc.sh >> /root/.bashrc
+    cat /tmp/bashrc.sh >> $USER_HOME/.bashrc
+}
--- infrastructure/README.md
+++ infrastructure/README.md
@@ -13,18 +13,7 @@ Steps to create:
   really exist. Set the DHCP options back to the DHCP options created by AWS (one with
   domain-name-servers = AmazonProvidedDNS).
 
-4. On the EC2 console select the NAT instance -> Actions -> Change Source/Dest. Check -> Yes, Disable.
-
-  NOTE: No known fix for CF scripts. May be other scripting solutions.
-
-5. On each jump box install the internal ssh key:
-
-  ```bash
-  vi ~/.ssh/id_rsa       # Paste in key to new file
-  chmod go-wrx ~/.ssh/id_rsa
-  ```
-
-6. Connect to DNS instance and execute the following:
+4. Connect to DNS instance and execute the following:
 
   ```bash
   sudo chown named:named /var/log/named
@@ -32,10 +21,10 @@ Steps to create:
 
   TODO: CF init should be able to resolve the chown issue eventually.
 
-7. Restarting is only necessary to make the previous change take effect.
+5. Restarting is only necessary to make the previous change take effect.
 
   ```bash
   sudo /etc/init.d/named restart
   ```
 
-8. Create Private Infrastructure stack [`private-infrastructure.template`](./private-infrastructure.template) (S3 URL [here](https://s3.amazonaws.com/aws-musings-us-east-1/infrastructure/private-infrastructure.template).
+6. Create Private Infrastructure stack [`private-infrastructure.template`](./private-infrastructure.template) (S3 URL [here](https://s3.amazonaws.com/aws-musings-us-east-1/infrastructure/private-infrastructure.template).
