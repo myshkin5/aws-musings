f9141ed083e76617ed5a50834cb5af4dbc46a8de
Dwayne Schultz <dschultz@pivotal.io>
2016-02-15 08:54:30 -0700

    Clean up output of full stack script

--- infrastructure/scripts/create-full-stack.sh
+++ infrastructure/scripts/create-full-stack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -e
 
 SCRIPTS_DIR=$(dirname $0)
 
@@ -8,11 +8,15 @@ trap "exit 1" TERM
 export TOP_PID=$$
 
 run-and-check() {
+    >&2 echo "Executing $*..."
     VARS=$($*)
     if [[ $? != 0 ]] ; then
         kill -s TERM $TOP_PID
     fi
     echo $VARS
+    >&2 echo "Complete. Generated output:"
+    >&2 echo $VARS
+    >&2 echo
 }
 
 eval $(run-and-check $SCRIPTS_DIR/create-vpc.sh $@)
