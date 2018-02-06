084ec912b7c2b03233b664631d1c9be3d48c3b59
Dwayne Schultz <dwayne@schultz.net>
2018-02-05 18:56:27 -0700

    Cut bash prompt to single line

--- common/bashrc.sh
+++ common/bashrc.sh
@@ -28,22 +28,11 @@ export TODO="t"
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
 
-SCM_THEME_PROMPT_DIRTY=" ${red}✗"
-SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-SCM_THEME_PROMPT_PREFIX=" |"
-SCM_THEME_PROMPT_SUFFIX="${green}|"
-
-GIT_THEME_PROMPT_DIRTY=" ${red}✗"
-GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-GIT_THEME_PROMPT_PREFIX=" ${green}|"
-GIT_THEME_PROMPT_SUFFIX="${green}|"
-
-RVM_THEME_PROMPT_PREFIX="|"
-RVM_THEME_PROMPT_SUFFIX="|"
-
 function prompt_command() {
-    PS1="\n$(clock_char) ${purple}\u@\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+    PS1="$(clock_prompt)${yellow}|\u@\h|${green}\w ${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${green}→${reset_color} "
 }
+
+export THEME_CLOCK_FORMAT="%dT%H:%M:%S"
 
 PROMPT_COMMAND=prompt_command;
 
